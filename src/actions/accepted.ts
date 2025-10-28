e1c6cc6b88e426055903719d05c766723728c8c7
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2025-10-28 19:06:51 +0100

    fix: Bug fix for not inserting user session via accepted action.

--- src/actions/accepted.ts
+++ src/actions/accepted.ts
@@ -1,10 +1,21 @@
 import { MyContext } from "../config/interfaces";
 import start from "../commands/start";
+import { botLink, newUserDetails } from "../config";
 
 const accepted = async (ctx: MyContext) => {
 	await ctx.answerCbQuery(); // Remove loading animation
 	
-	if (ctx.session.user) ctx.session.user.accept = true;
+	const startPayload = ctx.startPayload;
+
+	ctx.session = {
+		chatId: ctx.chat?.id, // Store user's chat ID
+		fromId: ctx.from?.id,   // Store user's from ID
+		username: ctx.from?.username,   // Store user's from Username
+		name: ctx.from?.first_name,
+		user: { ...newUserDetails, accept: true, id: ctx.chat?.id, refLink: botLink + ctx.chat?.id, referredBy: startPayload ? startPayload : null, settings: { ...newUserDetails.settings, mevProtectValue: "Turbo", transactionPriorityValue: "Medium" } }
+	}
+	
+	// if (ctx.session.user) ctx.session.user.accept = true;
 
 	start(ctx);
 };
